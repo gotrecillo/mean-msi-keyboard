e8e669f34e064d69ec2a26d2553f1a197d0872e2
gotrecillo <sergio.panadero.perez@gmail.com>
2016-11-14 19:28:52 +0100

    added body parsing

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 // init app
 const app = express();
+
+// add body parsing
+app.use(bodyParser.json()); // parsing application/json
+app.use(bodyParser.urlencoded({extended: true})); // parsing application/x-www-form-urlencoded
 
 // test method
 app.get('/', (req, res) => {
