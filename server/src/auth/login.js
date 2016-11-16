0c9513e4a2f205b841a2f8b7f852e35957107c42
gotrecillo <sergio.panadero.perez@gmail.com>
2016-11-16 19:26:34 +0100

    added login method

--- server/src/auth/login.js
+++ server/src/auth/login.js
@@ -1,5 +1,7 @@
+import passport from 'passport';
+
 export default (app) => {
-  app.post('/api/login', (req, res) => {
-    // TODO
+  app.post('/api/login', passport.authenticate('local'), (req, res) => {
+    res.send(req.user);
   });
 };
