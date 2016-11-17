d0378d601db10c3888f79183ae595bc9d6b6f826
gotrecillo <sergio.panadero.perez@gmail.com>
2016-11-17 16:33:48 +0100

    send jwt after login, add tests for it

--- server/test/login.js
+++ server/test/login.js
@@ -1,8 +1,10 @@
 // npm packages
 import request from 'supertest';
+import jwt from 'jsonwebtoken';
 
 // our packages
 import app from '../src/app';
+import { auth as authConfig } from '../config';
 
 export default (test) => {
   test('Should login with existing username and password', t => {
@@ -13,10 +15,14 @@ export default (test) => {
       .expect('Content-Type', /json/)
       .end((err, res) => {
         const actualBody = res.body;
+        const decodedUser = jwt.verify(actualBody.token, authConfig.jwtSecret);
+        delete decodedUser.iat; // ignore jwt timestamps
 
         t.error(err, 'No error');
         t.ok(actualBody.user, 'User exists');
+        t.ok(actualBody.token, 'Token exists');
         t.equal(actualBody.user.username, 'foo', 'Username matches request');
+        t.deepEqual(actualBody.user, decodedUser, 'User must match token');
         t.end();
       })
   });
